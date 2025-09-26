6511163a580864c5233ab88b2f036b1a23a2a3a5
Tempy <usedtemplate@gmail.com>
2025-09-26 20:27:34 +0200

    Add mapbox_maps_flutter and flutter_dotenv support
    Added mapbox_maps_flutter and flutter_dotenv dependencies to pubspec.yaml and updated asset configuration to include .env. Modified main.dart to load environment variables from .env at startup and print the ACCESS_TOKEN. Updated IDE configuration files to recognize new packages.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async  {
+  WidgetsFlutterBinding.ensureInitialized(); // Ensure Flutter is initialized
+  try {
+    await dotenv.load(fileName: ".env"); // Load environment variables
+  } catch (e) {
+    throw Exception('Error loading .env file: $e'); // Print error if any
+  }
+
+  final String apiKey = dotenv.env['ACCESS_TOKEN'] ?? 'default_key';
+  print(apiKey);
+
   runApp(const CarDashboardApp());
 }
 
