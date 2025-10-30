ec769dd85ac76bc62ca29f071cc5a6449ef08f3e
Joost <2006joost@gmail.com>
2025-10-30 20:55:53 +0100

    wifi

--- lib/layouts/providers_island_layout.dart
+++ lib/layouts/providers_island_layout.dart
@@ -1,3 +1,4 @@
+import 'package:car_dashboard/services/wifi_service.dart';
 import 'package:flutter/material.dart';
 
 class ProvidersIslandLayout extends StatefulWidget {
@@ -8,27 +9,49 @@ class ProvidersIslandLayout extends StatefulWidget {
 }
 
 class _ProvidersIslandLayoutState extends State<ProvidersIslandLayout> {
-  late String currentTime;
+  final WifiService _wifiService = WifiService();
+
+  @override
+  void initState() {
+    super.initState();
+    _wifiService.checkForInternet();
+  }
+
+  @override
+  void dispose() {
+    _wifiService.hasInternet.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Positioned(
       top: 15,
       right: 15,
-      child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 8),
-          decoration: BoxDecoration(
-            color: const Color(0xFF1C1C1C),
-            borderRadius: BorderRadius.circular(30),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.4),
-                blurRadius: 12,
-                offset: const Offset(0, 4),
-              )
-            ],
-          ),
-          child: Icon(Icons.wifi_off)),
+      child: ValueListenableBuilder<bool>(
+        valueListenable: _wifiService.hasInternet,
+        builder: (context, hasInternet, child) {
+          return Container(
+            padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 8),
+            decoration: BoxDecoration(
+              color: const Color(0xFF1C1C1C),
+              borderRadius: BorderRadius.circular(30),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.4),
+                  blurRadius: 12,
+                  offset: const Offset(0, 4),
+                )
+              ],
+            ),
+            child: Icon(
+              hasInternet ? Icons.wifi : Icons.wifi_off,
+              size: 20,
+              color: hasInternet ? Colors.green : Colors.red,
+            ),
+          );
+        },
+      ),
     );
   }
 }
---
+++ lib/services/wifi_service.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+import 'dart:developer';
+import 'package:flutter/foundation.dart';
+
+class WifiService {
+  // Singleton pattern
+  static final WifiService _instance = WifiService._internal();
+  factory WifiService() => _instance;
+  WifiService._internal();
+
+  final ValueNotifier<bool> hasInternet = ValueNotifier(true);
+  bool _isChecking = false;
+
+  Future<void> checkForInternet() async {
+    if (_isChecking) return; // Prevent multiple loops
+    _isChecking = true;
+
+    while (true) {
+      try {
+        await Future.delayed(const Duration(seconds: 5));
+        final result = await InternetAddress.lookup('google.com');
+        if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+          hasInternet.value = true;
+          log('connected: ${hasInternet.value}');
+        }
+      } on SocketException catch (_) {
+        hasInternet.value = false;
+        log('not connected: ${hasInternet.value}');
+      }
+    }
+  }
+}
